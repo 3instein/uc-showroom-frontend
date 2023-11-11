b3067ab0443417ee2a2091fca324ace2d17dab1c
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2023-11-11 17:36:27 +0700

    Add CustomerResponse interface to include complete customer data

--- src/interfaces/Customer.ts
+++ src/interfaces/Customer.ts
@@ -42,6 +42,20 @@ export interface Customer {
 }
 
 /**
+ * Represents the response data for a customer, including the personal details.
+ *
+ * @interface CustomerResponse
+ * @extends {Customer}
+ */
+export interface CustomerResponse {
+    /**
+     * The complete data of the customer.
+     * @type {Customer}
+     */
+    data: Customer;
+}
+
+/**
  * Represents the data required to create a new customer, excluding the unique identifier.
  *
  * @type CreateCustomer
