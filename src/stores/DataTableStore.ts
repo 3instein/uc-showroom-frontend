763baf523f530cb344c1bdbd99356374fa30f0e8
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2023-11-11 16:56:49 +0700

    Add documentations for the store

--- src/stores/DataTableStore.ts
+++ src/stores/DataTableStore.ts
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
+// Import the create function from 'zustand'
 import { create } from 'zustand';
 
+// Define the types for the store state
 type DataTableState = {
     tableData: any[];
     filteredTableData: any[];
@@ -10,21 +12,26 @@ type DataTableState = {
     setSelectedFilter: (filterType: string, value: string) => void;
 }
 
+// Create and export the useDataTableStore hook
 export const useDataTableStore = create<DataTableState>((set) => ({
+    // Initial state with empty tableData, filteredTableData, and default selected filter
     tableData: [],
     filteredTableData: [],
     selectedFilters: {
-        status: '',
+        status: '', // Default selected filter (example: 'status')
     },
 
+    // Function to set the tableData state
     setTableData: (tableData) => {
         set({ tableData });
     },
 
+    // Function to set the filteredTableData state
     setFilteredTableData: (filteredTableData) => {
         set({ filteredTableData });
     },
 
+     // Function to set the selectedFilters state based on filterType and value
     setSelectedFilter: (filterType, value) => {
         set(state => ({
             selectedFilters: {
