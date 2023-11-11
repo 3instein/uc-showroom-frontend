889bcd7a378d172b0eaa441f0bd8e6b2d1bf5530
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2023-11-11 17:37:02 +0700

    Update CustomerCRUD.ts to return CustomerResponse instead of Customer

--- src/api/CustomerCRUD.ts
+++ src/api/CustomerCRUD.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse } from "axios";
-import { CreateCustomer, Customer } from "../interfaces/Customer";
+import { CreateCustomer, Customer, CustomerResponse } from "../interfaces/Customer";
 
 const BASE_URL = "http://localhost:3000";
 
@@ -7,10 +7,10 @@ const BASE_URL = "http://localhost:3000";
  * Creates a new customer with the provided information.
  *
  * @param {CreateCustomer} customer - The customer data for creating a new customer.
- * @returns {Promise<AxiosResponse<Customer>>} - A Promise that resolves to the AxiosResponse containing the created customer.
+ * @returns {Promise<AxiosResponse<CustomerResponse>>} - A Promise that resolves to the AxiosResponse containing the created customer.
  * @throws {Error} - Throws an error if the creation process fails.
  */
-export const createCustomer = async (customer: CreateCustomer): Promise<AxiosResponse<Customer>> => {
+export const createCustomer = async (customer: CreateCustomer): Promise<AxiosResponse<CustomerResponse>> => {
     try {
         const response = await axios.post(`${BASE_URL}/customers`, customer);
         return response;
@@ -24,10 +24,10 @@ export const createCustomer = async (customer: CreateCustomer): Promise<AxiosRes
  * Updates an existing customer with the provided information.
  *
  * @param {Customer} customer - The updated customer data.
- * @returns {Promise<AxiosResponse<Customer>>} - A Promise that resolves to the AxiosResponse containing the updated customer.
+ * @returns {Promise<AxiosResponse<CustomerResponse>>} - A Promise that resolves to the AxiosResponse containing the updated customer.
  * @throws {Error} - Throws an error if the update process fails.
  */
-export const updateCustomer = async (customer: Customer): Promise<AxiosResponse<Customer>> => {
+export const updateCustomer = async (customer: Customer): Promise<AxiosResponse<CustomerResponse>> => {
     try {
         const response = await axios.put(`${BASE_URL}/customers/${customer.id}`, customer);
         return response;
