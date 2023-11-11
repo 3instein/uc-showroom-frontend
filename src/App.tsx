8c62e03bedca4213cb5d08a5b5ba1fb82455b4f7
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2023-11-11 16:39:14 +0700

    Add comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
+// Import necessary dependencies from React and React Router
 import './App.css'
 import { Outlet, NavLink } from "react-router-dom";
 
+// Define the main App component
 function App() {
+  // Render the component
   return (
     <>
+      {/* Hero section */}
       <div className="hero min-h-screen">
         <div className="hero-content text-center">
           <div className='w-screen'>
+            {/* Main title */}
             <h1 className="text-5xl font-bold">UC Showroom</h1>
+            {/* Subtitle */}
             <p className="py-6 text-2xl">Pilih salah satu menu di bawah untuk memulai!</p>
+            {/* Navigation tabs */}
             <div className="tabs tabs-boxed">
+              {/* Customer Tab */}
               <NavLink
                 to={`customers`}
                 className={({ isActive, isPending }) =>
@@ -22,6 +30,7 @@ function App() {
               >
                 <button className='text-lg font-semibold'>Pelanggan</button>
               </NavLink>
+              {/* Cars Tab */}
               <NavLink
                 to={`cars`}
                 className={({ isActive, isPending }) =>
@@ -34,6 +43,7 @@ function App() {
               >
                 <button className='text-lg font-semibold'>Mobil</button>
               </NavLink>
+              {/* Trucks Tab */}
               <NavLink
                 to={`trucks`}
                 className={({ isActive, isPending }) =>
@@ -46,6 +56,7 @@ function App() {
               >
                 <button className='text-lg font-semibold'>Truk</button>
               </NavLink>
+              {/* Motorcycles Tab */}
               <NavLink
                 to={`motorcycles`}
                 className={({ isActive, isPending }) =>
@@ -58,6 +69,7 @@ function App() {
               >
                 <button className='text-lg font-semibold'>Motor</button>
               </NavLink>
+              {/* Orders Tab */}
               <NavLink
                 to={`orders`}
                 className={({ isActive, isPending }) =>
@@ -71,6 +83,7 @@ function App() {
                 <button className='text-lg font-semibold'>Pesanan</button>
               </NavLink>
             </div>
+            {/* Render the nested routes */}
             <Outlet />
           </div>
         </div>
@@ -79,4 +92,5 @@ function App() {
   )
 }
 
+// Export the App component as the default export
 export default App;
