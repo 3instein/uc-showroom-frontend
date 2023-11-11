afe7e1ce23ea2e3d85891c1edda4d69988f04cba
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2023-11-11 17:20:25 +0700

    Add documentations to CustomerCRUD

--- src/api/CustomerCRUD.ts
+++ src/api/CustomerCRUD.ts
@@ -1,16 +1,55 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { CreateCustomer, Customer } from "../interfaces/Customer";
 
 const BASE_URL = "http://localhost:3000";
 
-export const createCustomer = async (customer: CreateCustomer) => {
-    return await axios.post(`${BASE_URL}/customers`, customer);
-}
+/**
+ * Creates a new customer with the provided information.
+ *
+ * @param {CreateCustomer} customer - The customer data for creating a new customer.
+ * @returns {Promise<AxiosResponse<Customer>>} - A Promise that resolves to the AxiosResponse containing the created customer.
+ * @throws {Error} - Throws an error if the creation process fails.
+ */
+export const createCustomer = async (customer: CreateCustomer): Promise<AxiosResponse<Customer>> => {
+    try {
+        const response = await axios.post(`${BASE_URL}/customers`, customer);
+        return response;
+    } catch (error) {
+        // Handle and rethrow the error for better error handling in the application.
+        throw new Error(`Failed to create customer`);
+    }
+};
 
-export const updateCustomer = async (customer: Customer) => {
-    return await axios.put(`${BASE_URL}/customers/${customer.id}`, customer);
-}
+/**
+ * Updates an existing customer with the provided information.
+ *
+ * @param {Customer} customer - The updated customer data.
+ * @returns {Promise<AxiosResponse<Customer>>} - A Promise that resolves to the AxiosResponse containing the updated customer.
+ * @throws {Error} - Throws an error if the update process fails.
+ */
+export const updateCustomer = async (customer: Customer): Promise<AxiosResponse<Customer>> => {
+    try {
+        const response = await axios.put(`${BASE_URL}/customers/${customer.id}`, customer);
+        return response;
+    } catch (error) {
+        // Handle and rethrow the error for better error handling in the application.
+        throw new Error(`Failed to update customer`);
+    }
+};
 
-export const deleteCustomer = async (id: number) => {
-    return await axios.delete(`${BASE_URL}/customers/${id}`);
-}
+/**
+ * Deletes a customer with the specified ID.
+ *
+ * @param {number} id - The ID of the customer to be deleted.
+ * @returns {Promise<AxiosResponse>} - A Promise that resolves to the AxiosResponse after successfully deleting the customer.
+ * @throws {Error} - Throws an error if the deletion process fails.
+ */
+export const deleteCustomer = async (id: number): Promise<AxiosResponse> => {
+    try {
+        const response = await axios.delete(`${BASE_URL}/customers/${id}`);
+        return response;
+    } catch (error) {
+        // Handle and rethrow the error for better error handling in the application.
+        throw new Error(`Failed to delete customer`);
+    }
+};
