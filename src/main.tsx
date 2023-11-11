84ac5261cfb6f027e7cc9292f1a8729638e95823
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2023-11-11 16:40:08 +0700

    Add comments

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+// Import React and ReactDOM for rendering
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// Import the main App component and necessary routing components
+import App from './App.tsx';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+
+// Import individual page components
 import { Cars } from './pages/Cars.tsx';
 import { Customers } from './pages/Customers.tsx';
 import { Trucks } from './pages/Trucks.tsx';
 import { Motorcycles } from './pages/Motorcycles.tsx';
 import { Orders } from './pages/Orders.tsx';
 
+// Create a BrowserRouter with defined routes
 const router = createBrowserRouter([
   {
     path: "/",
@@ -40,8 +46,9 @@ const router = createBrowserRouter([
   },
 ]);
 
+// Use ReactDOM.createRoot to render the RouterProvider with the router
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
